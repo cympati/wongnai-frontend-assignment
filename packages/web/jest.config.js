52cd47af7135f3332d9329040cf162c043700466
Ms.PATIPHON KLANGPRAPHAN <63130500216@st.sit.kmutt.ac.th>
2023-01-22 21:48:50 +0700

    setup fails

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,46 +1,13 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/mocks/**",
+  preset: "vite-jest",
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
   ],
-  coveragePathIgnorePatterns: [],
-  setupFilesAfterEnv: ["./config/jest/setupTests.js"],
-  testEnvironment: "jsdom",
-  modulePaths: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
-      "<rootDir>/config/jest/fileTransform.js",
-  },
-  transformIgnorePatterns: [
-    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
-    "^.+\\.module\\.(css|sass|scss)$",
-  ],
-  modulePaths: ["<rootDir>/src"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    "^react-native$": "react-native-web",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "\\.(css|sass|scss)$": "identity-obj-proxy",
   },
-  moduleFileExtensions: [
-    // Place tsx and ts to beginning as suggestion from Jest team
-    // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
-    "tsx",
-    "ts",
-    "web.js",
-    "js",
-    "web.ts",
-    "web.tsx",
-    "json",
-    "web.jsx",
-    "jsx",
-    "node",
-  ],
-  watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname",
-  ],
-  resetMocks: true,
 };
